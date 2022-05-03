b7ac51957ab3016cde87c666222a3fa03e963e97
Estevan Fonseca <estevanoliveira21@gmail.com>
2022-05-03 15:25:36 -0300

    Tabelas REH -> até tarifas de aplicação

--- main/config.py
+++ main/config.py
@@ -2,4 +2,4 @@ import os
 
 FILES = os.getcwd() + "\\files"
 FILES2 = r'C:\Users\estev\Urca Energia Participações Ltda\Communication site - Comercializadora\Estudos\Autoprodução\SIMULADOR VR\Tarifas\PCAT' #os.getcwd() + "\\files2"
-CHROMEDRIVER = os.path.dirname(os.getcwd()) + "\\Tarifas\\chromedriver\\chromedriver.exe" # fix
+CHROMEDRIVER = r'C:\Users\estev\Documents\Tarifas\chromedriver\chromedriver.exe'
